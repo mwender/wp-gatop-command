ee76524c841ed8ddbe91eb35148b11e02ad48dba
Michael Wender <michael@michaelwender.com>
2024-12-23 16:30:31 -0500

    Including class file

--- bin/wp-gatop-command.php
+++ bin/wp-gatop-command.php
@@ -4,5 +4,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../src/GATopCommand.php';
 
 WP_CLI::add_command( 'gatop', 'MWender\\GATopCommand\\GATopCommand' );
